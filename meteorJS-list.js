0001f504f83e6d565d8e4b3b41583676e29211f0
TKais <TiffanyTKaiser@gmail.com>
2015-02-15 11:11:07 -0800

    Make a form to create todo tasks

--- meteorJS-list.js
+++ meteorJS-list.js
@@ -7,4 +7,18 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .add-todos": function(event){    //submitting the form
+      var userInput = event.target.value;         //create text variable to hold user's input
+
+      Tasks.insert({                         //insert tasks into mongo
+        text: userInput,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";          //clear the form
+
+      return false;                          //prevent default form submit
+    }
+  });
 }
